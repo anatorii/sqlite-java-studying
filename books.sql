a73163e344587158edb2cb83f085cd1411b1e800
anatoly.igumnov <anatoly.igumnov@gmail.com>
2023-09-25 20:21:03 +1100

    insert data script

---
+++ books.sql
@@ -0,0 +1,7 @@
+INSERT INTO genres (id, name) VALUES (1, 'детектив');
+INSERT INTO genres (id, name) VALUES (2, 'фантастика');
+INSERT INTO genres (id, name) VALUES (3, 'драма');
+
+INSERT INTO books (id, name, author, year, genre_id) VALUES (1, 'Конец Вечности', 'Азимов, Айзек', 1976, 1);
+INSERT INTO books (id, name, author, year, genre_id) VALUES (2, 'Камешек в небе', 'Азимов, Айзек', 1972, 2);
+INSERT INTO books (id, name, author, year, genre_id) VALUES (3, 'Сами боги', 'Азимов, Айзек', 1980, 3);
